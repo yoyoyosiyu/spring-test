d885849507bf033f7aa70bdabb58994cd669f687
yoyoyosiyu <yoyoyosiyu@126.com>
2019-05-29 01:21:29 +0800

    增加了spring-json模块，验证实体与Json转换

---
+++ spring-jpa-multiselect/src/main/java/com/huayutech/springjpamultiselect/SpringJpaMultiselectApplication.java
@@ -0,0 +1,13 @@
+package com.huayutech.springjpamultiselect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaMultiselectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaMultiselectApplication.class, args);
+	}
+
+}
